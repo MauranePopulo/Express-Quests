15310377d0d52c8a22a808d932d6841bb8e420ac
Maurane Populo <maurane.populo@orange.fr>
2023-09-26 21:42:02 +0200

    afficher les users

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,3 +1,5 @@
+const database = require("./database");
+
 const movies = [
   {
     id: 1,
@@ -29,17 +31,23 @@ const getMovies = (req, res) => {
   res.json(movies);
 };
 
-const getMovieById = (req, res) => {
-  const id = parseInt(req.params.id);
-
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
-};
+  const getMovieById = (req, res) => {
+    const id = parseInt(req.params.id);
+  
+    database
+      .query("select * from movies where id = ?", [id])
+      .then(([movies]) => {
+        if (movies[0] != null) {
+          res.json(movies[0]);
+        } else {
+          res.status(404).send("Not Found");
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error retrieving data from database");
+      });
+  };
 
 module.exports = {
   getMovies,

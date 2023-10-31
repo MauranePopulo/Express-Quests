22cdd8f9faa07b9c9163d65f76c4845cd158b15d
Maurane Populo <maurane.populo@orange.fr>
2023-10-31 12:32:55 +0100

    création d'un token avec mdp haché, puis authentification avec JWT

--- userHandlers.js
+++ userHandlers.js
@@ -41,7 +41,7 @@ const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-    .query("select firstname, lastname, email, city, language from users where id = ?", [id])
+    .query("select firstname, lastname, email, city, language, hashedPassword from users where id = ?", [id])
     .then(([users]) => {
       if (users[0] != null) {
         res.json(users[0]);
@@ -113,10 +113,31 @@ const deleteUser = (req, res) => {
     });
 };
 
+const getUserByEmailWithPasswordAndPassToNext = (req, res, next) => {
+  const { email } = req.body;
+
+  database
+    .query("select * from users where email = ?", [email])
+    .then(([users]) => {
+      if (users[0] != null) {
+        req.user = users[0];
+
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
   updateUser,
   deleteUser,
+  getUserByEmailWithPasswordAndPassToNext,
 };
